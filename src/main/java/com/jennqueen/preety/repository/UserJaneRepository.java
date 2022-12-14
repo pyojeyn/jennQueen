2b6d56a1d8537799145f6cc92f2774de8c5b9292
pyojeyn <jeynpyo@gmail.com>
2022-12-15 08:25:10 +0900

    repository 확장2

---
+++ src/main/java/com/jennqueen/preety/repository/UserJaneRepository.java
@@ -0,0 +1,8 @@
+package com.jennqueen.preety.repository;
+
+import com.jennqueen.preety.entity.User;
+
+import java.math.BigInteger;
+
+public interface UserJaneRepository extends JaneRepository<User, BigInteger>{
+}
